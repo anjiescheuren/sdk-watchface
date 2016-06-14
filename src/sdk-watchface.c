5267f2320f28ae87182994fb1f83fccad16399bc
Anjie Scheuren <emailanjie@gmail.com>
2016-06-14 15:00:12 -0700

    trying to fix font

--- src/sdk-watchface.c
+++ src/sdk-watchface.c
@@ -1,4 +1,5 @@
 #include <pebble.h>
+
 static Window *s_main_window;
 static TextLayer *s_time_layer;
 
@@ -30,6 +31,16 @@ static void main_window_load(Window *window) {
   Layer *window_layer = window_get_root_layer(window);
   GRect bounds = layer_get_bounds(window_layer);
 
+  // Create GBitmap
+  s_background_bitmap = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_BACKGROUND);
+
+  // Create BitmapLayer to display the GBitmap
+  s_background_layer = bitmap_layer_create(bounds);
+
+  // Set the bitmap onto the layer and add to the window
+  bitmap_layer_set_bitmap(s_background_layer, s_background_bitmap);
+  layer_add_child(window_layer, bitmap_layer_get_layer(s_background_layer));
+
   // Create the TextLayer with specific bounds
   s_time_layer = text_layer_create(
       GRect(0, PBL_IF_ROUND_ELSE(58, 52), bounds.size.w, 50));
@@ -38,11 +49,7 @@ static void main_window_load(Window *window) {
   text_layer_set_background_color(s_time_layer, GColorClear);
   text_layer_set_text_color(s_time_layer, GColorBlack);
   text_layer_set_text(s_time_layer, "00:00");
-  text_layer_set_font(s_time_layer, fonts_get_system_font(FONT_KEY_BITHAM_42_BOLD));
   text_layer_set_text_alignment(s_time_layer, GTextAlignmentCenter);
-
-  // Add it as a child layer to the Window's root layer
-  layer_add_child(window_layer, text_layer_get_layer(s_time_layer));
 
   // Create GFont
   s_time_font = fonts_load_custom_font(resource_get_handle(RESOURCE_ID_FONT_PERFECT_DOS_48));
@@ -50,17 +57,8 @@ static void main_window_load(Window *window) {
   // Apply to TextLayer
   text_layer_set_font(s_time_layer, s_time_font);
 
-  // Create GBitmap
-  s_background_bitmap = gbitmap_create_with_resource(RESOURCE_ID_IMAGE_BACKGROUND);
-
-
-  // Create BitmapLayer to display the GBitmap
-  s_background_layer = bitmaplayer_create(bounds);
-
-  // Set the bitmap onto the layer and add to the window
-  bitmap_layer_set_bitmap(s_background_layer, s_background_bitmap);
-  layer_add_child(window_layer, bitmap_layer_get_layer(s_background_layer));
-
+  // Add it as a child layer to the Window's root layer
+  layer_add_child(window_layer, text_layer_get_layer(s_time_layer));
 }
 
 static void main_window_unload(Window *window) {
@@ -68,19 +66,22 @@ static void main_window_unload(Window *window) {
   text_layer_destroy(s_time_layer);
 
   // Unload GFont
-  font_unload_custom_font(s_time_font);
+  fonts_unload_custom_font(s_time_font);
 
   // Destroy GBitmap
   gbitmap_destroy(s_background_bitmap);
 
   // Destroy BitmapLayer
   bitmap_layer_destroy(s_background_layer);
-
 }
 
+
 static void init() {
-// Create main Window element and assign to pointer
+  // Create main Window element and assign to pointer
   s_main_window = window_create();
+
+  // Set the background color
+  window_set_background_color(s_main_window, GColorBlack);
 
   // Set handlers to manage the elements inside the Window
   window_set_window_handlers(s_main_window, (WindowHandlers) {
@@ -96,12 +97,10 @@ static void init() {
 
   // Register with TickTimerService
   tick_timer_service_subscribe(MINUTE_UNIT, tick_handler);
-
-  window_set_background_color(s_main_window, GColorBlack);
 }
 
 static void deinit() {
-// Destroy Window
+  // Destroy Window
   window_destroy(s_main_window);
 }
 
